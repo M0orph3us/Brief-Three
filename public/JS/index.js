ce1b5909f9ef16d48745e9bdb3c27c61c08b3bea
Gael <gael.moreau.simplon@gmail.com>
2024-02-19 16:58:32 +0100

    Add icons

--- public/JS/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-// import { v4 as uuidv4 } from "uuid";
-
-// console.log(uuidv4());
---
+++ public/JS/index.mjs
@@ -0,0 +1,13 @@
+"use strict";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const currentPage = window.location.pathname;
+
+//   if (currentPage === "/public/pages/home.html") {
+//     import("./scripts/home.mjs");
+//   } else if (currentPage === "/public/pages/adminBoard.html") {
+//     import("./scripts/adminboard.mjs");
+//   } else if (currentPage === "/public/pages/newProperty.html") {
+//     import("./scripts/newProperty.mjs");
+//   }
+// });
