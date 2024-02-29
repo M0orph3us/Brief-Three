27f256f4a864dcbeb1e2cf978b4ee8a24ca69cad
Gaël <gael.moreau.simplon@gmail.com>
2024-02-29 19:33:50 +0100

    add createCards by products

--- public/JS/scripts/home.mjs
+++ public/JS/scripts/home.mjs
@@ -1,41 +1,116 @@
 import { LocalStorage } from "../class/LocalStorage.mjs";
+const storage = new LocalStorage();
 
-const test = new LocalStorage();
-// test.getStorageLand();
-// test.getStorageHouse();
-// test.getStorageApartment();
-// test.getAllStorage();
-const cardsListTarget = document.querySelector("#cards-list-container");
-test.getStorageLand().forEach((value) => {
-  console.log(value);
-  const createCardContainer = document.createElement("div");
-  createCardContainer.id = value.id;
-  createCardContainer.classList.add("card-container");
+function createCards(data) {
+  const cardsListTarget = document.querySelector("#cards-list-container");
+  for (const value of data) {
+    const createCardContainer = document.createElement("div");
+    createCardContainer.id = value.id;
+    createCardContainer.classList.add("card-container");
 
-  cardsListTarget.append(createCardContainer);
+    cardsListTarget.append(createCardContainer);
 
-  const cardContainerTarget = document.getElementById(value.id);
+    const cardContainerTarget = document.getElementById(value.id);
 
-  const createTitle = document.createElement("h3");
-  createTitle.textContent = value.title;
+    const createTitle = document.createElement("h3");
+    createTitle.textContent = value.title;
+    createTitle.classList.add("cards-tilte");
 
-  const createLocation = document.createElement("p");
-  createLocation.textContent = value.location;
+    const createLocation = document.createElement("p");
+    createLocation.textContent = value.location;
+    createLocation.classList.add("card-location");
 
-  const createDescription = document.createElement("p");
-  createDescription.textContent = value.description;
+    const createDescription = document.createElement("p");
+    createDescription.textContent = value.description;
+    createDescription.classList.add("card-description");
 
-  const createM2 = document.createElement("p");
-  createM2.textContent = value.m2;
+    const createM2 = document.createElement("p");
+    createM2.textContent = value.m2;
+    createM2.classList.add("card-m2");
 
-  const price = document.createElement("p");
-  price.textContent = value.price;
+    const createPrice = document.createElement("p");
+    createPrice.textContent = value.price;
+    createPrice.classList.add("card-price");
 
-  cardContainerTarget.append(
-    createTitle,
-    createLocation,
-    createDescription,
-    createM2,
-    price
-  );
-});
+    cardContainerTarget.append(
+      createTitle,
+      createLocation,
+      createDescription,
+      createM2,
+      createPrice
+    );
+  }
+}
+
+function createCardsHouse() {
+  const dataHouse = storage.getStorageHouse();
+  createCards(dataHouse);
+}
+
+function createCardsLand() {
+  const descriptionTarget = document.querySelector("#description-card");
+  const dataLand = storage.getStorageLand();
+  createCards(dataLand);
+  const createBuildable = document.createElement("p");
+  for (const value of dataLand) {
+    if (value.createBuildable === true) {
+      createBuildable.textContent = "Yes";
+    } else {
+      createBuildable.textContent = "No";
+    }
+    descriptionTarget.append(createBuildable);
+  }
+}
+
+function createCardsApartment() {
+  const dataApartment = storage.getStorageApartment();
+  createCards(dataApartment);
+  for (const value of dataApartment) {
+  }
+}
+
+function createCardsAllProducts() {
+  const allData = storage.getStorage();
+  for (const key in allData) {
+    if (Object.hasOwnProperty.call(allData, key)) {
+      const element = allData[key];
+      for (const value of element) {
+        const createCardContainer = document.createElement("div");
+        createCardContainer.id = value.id;
+        createCardContainer.classList.add("card-container");
+
+        cardsListTarget.append(createCardContainer);
+
+        const cardContainerTarget = document.getElementById(value.id);
+
+        const createTitle = document.createElement("h3");
+        createTitle.textContent = value.title;
+        createTitle.classList.add("cards-tilte");
+
+        const createLocation = document.createElement("p");
+        createLocation.textContent = value.location;
+        createLocation.classList.add("card-location");
+
+        const createDescription = document.createElement("p");
+        createDescription.textContent = value.description;
+        createDescription.classList.add("card-description");
+
+        const createM2 = document.createElement("p");
+        createM2.textContent = value.m2;
+        createM2.classList.add("card-m2");
+
+        const createPrice = document.createElement("p");
+        createPrice.textContent = value.price;
+        createPrice.classList.add("card-price");
+
+        cardContainerTarget.append(
+          createTitle,
+          createLocation,
+          createDescription,
+          createM2,
+          createPrice
+        );
+      }
+    }
+  }
+}
